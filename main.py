087df23c084da6f245de74b1d62cf2bfc3917842
Victor Ponce <vicmanuelr@gmail.com>
2022-09-21 17:30:30 -0600

    Countdown mechanism added
    - using window after for countdown
    - added timer variable for timer label
    - added countdown function
    - added start timer for button call start

--- main.py
+++ main.py
@@ -9,11 +9,30 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def start_stimer():
+    countdown(2 * 60)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def countdown(count):
+    count_min = count // 60
+    count_sec = count % 60
+    format_sec = str()
+    if count_sec < 10:
+        format_sec = f"0{count_sec}"
+    else:
+        format_sec = count_sec
+    canvas.itemconfig(timer_text, text=f"{count_min}:{format_sec}")
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Window related
@@ -28,7 +47,7 @@ title.grid(column=1, row=0)
 check_mark.grid(column=1, row=3)
 
 # Buttons
-start = Button(text="Start", bg=GREEN, font=FONT_NAME, activebackground=GREEN, highlightthickness=0)
+start = Button(text="Start", bg=GREEN, font=FONT_NAME, activebackground=GREEN, highlightthickness=0, command=start_stimer)
 reset = Button(text="Reset", bg=RED, font=FONT_NAME, activebackground=RED, highlightthickness=0)
 start.grid(column=0, row=2)
 reset.grid(column=2, row=2)
@@ -37,7 +56,7 @@ reset.grid(column=2, row=2)
 tomato_img = PhotoImage(file="tomato.png")
 canvas = Canvas(width=202, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(102, 112, image=tomato_img)
-canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 window.mainloop()
