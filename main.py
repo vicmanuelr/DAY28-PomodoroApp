0f6f46145b70dc74a6e4909299c97ccf0482998f
Victor Ponce <vicmanuelr@gmail.com>
2022-09-21 16:12:16 -0600

    Finished visual setup of the program
    - changed window padding
    - added labels and buttons
    - changed display for grid setup

--- main.py
+++ main.py
@@ -19,13 +19,25 @@ LONG_BREAK_MIN = 20
 # Window related
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=25, pady=50, bg=YELLOW)
+
+# Labels
+title = Label(text="Timer", font=(FONT_NAME, 35), bg=YELLOW, fg=GREEN, justify="center")
+check_mark = Label(text="✔", font=(FONT_NAME, 35), bg=YELLOW, fg=GREEN, justify="center")
+title.grid(column=1, row=0)
+check_mark.grid(column=1, row=3)
+
+# Buttons
+start = Button(text="Start", bg=GREEN, font=FONT_NAME, activebackground=GREEN, highlightthickness=0)
+reset = Button(text="Reset", bg=RED, font=FONT_NAME, activebackground=RED, highlightthickness=0)
+start.grid(column=0, row=2)
+reset.grid(column=2, row=2)
 
 # Canvas related
 tomato_img = PhotoImage(file="tomato.png")
 canvas = Canvas(width=202, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(102, 112, image=tomato_img)
 canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
 window.mainloop()
