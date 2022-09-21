4af70adc7ecf903f39841a902e94f15506e4992f
Victor Ponce <vicmanuelr@gmail.com>
2022-09-20 23:25:28 -0600

    Created window, canvas
    - added image tomato to canvas
    - bg defined by yellow constant for both canvas and window

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,16 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+# Window related
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# Canvas related
+tomato_img = PhotoImage(file="tomato.png")
+canvas = Canvas(width=202, height=224, bg=YELLOW, highlightthickness=0)
+canvas.create_image(102, 112, image=tomato_img)
+canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+window.mainloop()
